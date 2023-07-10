18fd227d934d5a5b47c0ad106a469922529d6042
Shisato Yano <shisatoyano@gmail.com>
2023-07-10 13:34:48 +0800

    defined main function

--- scripts/rosbog_to_json_converter.py
+++ scripts/rosbog_to_json_converter.py
@@ -1,4 +1,12 @@
-#!/usr/bin/python3
+#!/usr/bin/python
+import rospy
+
+def main():
+    print("rosbag_to_json_converter start")
+
+    rospy.init_node("rosbag_to_json_converter", anonymous=True)
+
+
 
 if __name__ == "__main__":
-    print("rosbag_to_json_converter start")
+    main()
